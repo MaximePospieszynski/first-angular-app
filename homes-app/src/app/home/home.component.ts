2d5f3d8b1e582a4165deccf0530859dd45e6f781
maxime <maxime.pospieszynski@gmail.com>
2023-12-07 17:49:19 +0100

    Nouvelle interface (exemples de données)

--- homes-app/src/app/home/home.component.ts
+++ homes-app/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HousingLocationComponent} from '../housing-location/housing-location.component';
+import {HousingLocation} from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,7 @@ import {HousingLocationComponent} from '../housing-location/housing-location.com
   template: `
     <section>
       <form>
-        <input type="text" placeholder="filter by city" />
+        <input type="text" placeholder="Filter by city" />
         <button class="primary" type="button">Search</button>
       </form>
     </section>
@@ -19,4 +20,17 @@ import {HousingLocationComponent} from '../housing-location/housing-location.com
   `,
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.dev/assets/tutorials/common';
+
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+}
